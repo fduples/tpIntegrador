f09ca1033f6a3b5041468b1e691c67525b97a7bb
Fade-tor <fade_tor@hotmail.com>
2023-04-25 15:03:33 -0300

    quitado de nav

---
+++ comedores.sql
@@ -0,0 +1,60 @@
+CREATE TABLE `test`.`remito` (
+   `id` INT NOT NULL AUTO_INCREMENT,
+   `instancia_id` INT NOT NULL,
+   `linea` INT NOT NULL,
+   `pliego` VARCHAR(255) NOT NULL,
+   `solicitante` VARCHAR(255) NOT NULL,
+   `remito` VARCHAR(255) NOT NULL,
+   `fecha` VARCHAR(255) NOT NULL,
+   `servicio` VARCHAR(255) NOT NULL,
+   `cantidad` VARCHAR(255) NOT NULL,
+   `orden_compra_id` INT NOT NULL,
+   `periodo_id` INT NOT NULL,
+   `created_at` DATETIME NOT NULL,
+   `updated_at` DATETIME NOT NULL,
+   `deleted_at` DATETIME NOT NULL,
+   PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE `test`.`orden_compra` (
+   `id` INT NOT NULL AUTO_INCREMENT,
+   `descripcion` VARCHAR(255) NOT NULL,
+   `fecha` DATETIME NOT NULL,
+   `ejercicio` VARCHAR(255) NOT NULL,
+   `numero` INT NOT NULL,
+   `proveedor_id` INT NOT NULL,
+   `pliego_id` INT NOT NULL,
+   `lastuser_id` INT NOT NULL,
+   `fecha_emision` DATETIME NOT NULL,
+   `created_at` DATETIME NOT NULL,
+   `updated_at` DATETIME NOT NULL,
+   `deleted_at` DATETIME NOT NULL,
+   `version` INT NOT NULL,
+   `solicitud` INT NOT NULL CHECK(solicitud IN (0, 1)),
+   PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE `test`.`proveedor` (
+   `id` INT NOT NULL AUTO_INCREMENT,
+   `nombre` VARCHAR(255) NOT NULL,
+   `cuit` INT NOT NULL,
+   `razon_social` VARCHAR(255) NOT NULL,
+   `numero` INT NOT NULL,
+   `fecha` DATETIME NOT NULL,
+   `ejercicio` VARCHAR(255) NOT NULL,
+   `pisoydepto` VARCHAR(255) NOT NULL,
+   `telefono` INT NOT NULL,
+   `calle` VARCHAR(255) NOT NULL,
+   `email` VARCHAR(255) NOT NULL,
+   `email_secundario` VARCHAR(255) NOT NULL,
+   `tipo_de_proveedor_id` INT NOT NULL,
+   `localidad_id` INT NOT NULL,
+   `sfguarduser_id` INT NOT NULL,
+   `created_at` DATETIME NOT NULL,
+   `updated_at` DATETIME NOT NULL,
+   `fecha_emision` DATETIME NOT NULL,
+   `version` INT NOT NULL,
+   `lastuser_id` INT NOT NULL,
+   `deleted_at` DATETIME NOT NULL,
+   PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
